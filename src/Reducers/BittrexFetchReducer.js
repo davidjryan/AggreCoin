7ba3df6d1cafc23dc2c21b981faa1fb82cb043b0
David Ryan <djrdev@icloud.com>
2017-12-21 10:05:08 -0700

    initial bittrex fetch reducer build

---
+++ src/Reducers/BittrexFetchReducer.js
@@ -0,0 +1,42 @@
+import {
+  FETCHING_BITTREX_DATA,
+  FETCHING_BITTREX_DATA_SUCCESS,
+  FETCHING_BITTREX_DATA_FAIL
+} from '../Utils/ActionTypes';
+
+const initialState = {
+  isLoading: false,
+  bittrexData: [],
+  hasErrored: false,
+  errorMessage: null
+};
+
+const BittrexFetchReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_BITTREX_DATA:
+      return { ...state,
+        isLoading: true,
+        bittrexData: null,
+        hasErrored: false,
+        errorMessage: null
+      };
+    case FETCHING_BITTREX_DATA_SUCCESS:
+      return { ...state,
+        isLoading: false,
+        bittrexData: action.payload,
+        hasErrored: false,
+        errorMessage: null
+      };
+    case FETCHING_BITTREX_DATA_FAIL:
+      return { ...state,
+        isLoading: false,
+        bittrexData: [],
+        hasErrored: true,
+        errorMessage: action.err
+      };
+    default:
+      return state;
+  }
+};
+
+export default BittrexFetchReducer;
