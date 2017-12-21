896b84d939f5a5a202833d2c82465528089c738c
David Ryan <djrdev@icloud.com>
2017-12-21 10:04:49 -0700

    initial Bittrex Fetch action build

---
+++ src/Actions/BittrexDataFetch.js
@@ -0,0 +1,23 @@
+import { 
+  FETCHING_BITTREX_DATA,
+  FETCHING_BITTREX_DATA_SUCCESS,
+  FETCHING_BITTREX_DATA_FAIL
+} from '../Utils/ActionTypes';
+
+export default function BittrexDataFetch() {
+  return dispatch => {
+
+    dispatch({ type: FETCHING_BITTREX_DATA })
+
+    return fetch('https://bittrex.com/api/v1.1/public/getorderbook?market=BTC-ETH&type=both')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        return dispatch({ type: FETCHING_BITTREX_DATA_SUCCESS, payload: res.data });
+      })
+      .catch(err => {
+        return dispatch({ type: FETCHING_BITTREX_DATA_FAIL, payload: err });
+      });
+
+  }
+}
