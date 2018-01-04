5dbaa518fe1b534628da035f33bb4a8273c8882b
David Ryan <djrdev@icloud.com>
2018-01-04 03:09:46 -0700

    linting

--- src/Reducers/BittrexFetchReducer.js
+++ src/Reducers/BittrexFetchReducer.js
@@ -1,38 +1,41 @@
 import {
   FETCHING_BITTREX_DATA,
   FETCHING_BITTREX_DATA_SUCCESS,
-  FETCHING_BITTREX_DATA_FAIL
+  FETCHING_BITTREX_DATA_FAIL,
 } from '../Utils/ActionTypes';
 
 const initialState = {
   isLoading: false,
   bittrexData: [],
   hasErrored: false,
-  errorMessage: null
+  errorMessage: null,
 };
 
 const BittrexFetchReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCHING_BITTREX_DATA:
-      return { ...state,
+      return {
+        ...state,
         isLoading: true,
         bittrexData: null,
         hasErrored: false,
-        errorMessage: null
+        errorMessage: null,
       };
     case FETCHING_BITTREX_DATA_SUCCESS:
-      return { ...state,
+      return {
+        ...state,
         isLoading: false,
         bittrexData: action.payload,
         hasErrored: false,
-        errorMessage: null
+        errorMessage: null,
       };
     case FETCHING_BITTREX_DATA_FAIL:
-      return { ...state,
+      return {
+        ...state,
         isLoading: false,
         bittrexData: [],
         hasErrored: true,
-        errorMessage: action.payload
+        errorMessage: action.payload,
       };
     default:
       return state;
