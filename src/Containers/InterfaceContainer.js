2ec5a66bb9c72b762255680ed739aac684174512
David Ryan <djrdev@icloud.com>
2018-01-06 21:59:29 -0700

    init interface container

--- src/Containers/InterfaceContainer.js
+++ src/Containers/InterfaceContainer.js
@@ -1,1 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class InterfaceContainer extends Component {
+  render() {
+    return (
+      <section>
+        <button></button>
+        <button></button>
+      </section>
+    )
+  }
+}
+
+export default InterfaceContainer;
