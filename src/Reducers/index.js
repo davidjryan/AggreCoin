80afc1b8bfa5dd285288dcd87666bed78e55f1a3
David Ryan <djrdev@icloud.com>
2018-01-07 00:02:48 -0700

    minor reducer changes

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import BittrexFetchReducer from './BittrexFetchReducer';
-import PoloniexFetchReducer from './PoloniexFetchReducer';
+import BittrexFetchReducer from '../BittrexFetchReducer/BittrexFetchReducer';
+import PoloniexFetchReducer from '../PoloniexFetchReducer/PoloniexFetchReducer';
 
 export default combineReducers({
   bittrex: BittrexFetchReducer,
-  poloniex: PoloniexFetchReducer
+  poloniex: PoloniexFetchReducer,
 });
