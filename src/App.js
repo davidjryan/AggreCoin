a6b5c587e367f313ce29e842615e3c5fc00b2e24
David Ryan <djrdev@icloud.com>
2018-01-06 20:36:30 -0700

    started component build in app

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import BittrexDataFetch from './Actions/BittrexDataFetch';
-import PoloniexDataFetch from './Actions/PoloniexDataFetch';
-
-import logo from './logo.svg';
+import GraphContainer from './Containers/GraphContainer';
+import InterfaceContainer from './Containers/InterfaceContainer';
 import './App.css';
 
 class App extends Component {
 
-  componentDidMount() {
-    this.props.BittrexDataFetch()
-    this.props.PoloniexDataFetch()
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <header />
+        <main>
+          <GraphContainer />
+          {/* <InterfaceContainer /> */}
+        </main>
       </div>
     );
   }
 }
 
 
-const mapStateToProps = (state) => {
-  const { bittrexData, hasErrored, isLoading, errorMessage } = state.bittrex;
-
-  return {
-    isLoading,
-    bittrexData,
-    hasErrored,
-    errorMessage
-  };
-};
-
-export default connect(mapStateToProps, { BittrexDataFetch, PoloniexDataFetch })(App);
+export default App;
