fad70ad6572a8a4e4ddd5d6025a95a85f923fd25
David Ryan <djrdev@icloud.com>
2018-01-06 20:37:47 -0700

    built out graph with victory line

--- src/Containers/GraphContainer.js
+++ src/Containers/GraphContainer.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
   VictoryStack,
-  VictoryArea,
+  VictoryLine,
   VictoryChart,
   VictoryTheme,
+  VictoryZoomContainer,
 } from 'victory';
 
 import BittrexDataFetch from '../Actions/BittrexDataFetch';
 import PoloniexDataFetch from '../Actions/PoloniexDataFetch';
 
 class GraphContainer extends Component {
+  
   // combineData() {
   //   const aggregateData = []
   //   return _.range(5).map(() => {
@@ -24,37 +26,94 @@ class GraphContainer extends Component {
   //   });
   // }
 
-  
-  componentDidMount() {
+
+  componentWillMount() {
     this.props.BittrexDataFetch();
     this.props.PoloniexDataFetch();
   }
 
-  cleanPoloniex() {
-    return 
+  bittrexBuy() {
+    const { buy } = this.props.bittrex.bittrexData;
+    const buyTruncated = buy.splice(0, 50)
+    const buyData = buyTruncated.map((order) => {
+      return {
+        x: order.Rate,
+        y: order.Quantity,
+      };
+    });
+
+    return buyData
   }
 
+  bittrexSell() {
+    const { sell } = this.props.bittrex.bittrexData;
+    const sellTruncated = sell.splice(0, 50)
+    const sellData = sellTruncated.map((order) => {
+      return {
+        x: order.Rate,
+        y: order.Quantity,
+      };
+    });
+
+    return sellData;
+  }
+
+  poloniexBuy() {
+    const { bids } = this.props.poloniex.poloniexData;
+    
+    const buyData = bids.map((order) => {
+      return {
+        x: parseFloat(order[0]),
+        y: order[1],
+      };
+    });
+
+    return buyData;
+  }
+
+  poloniexSell() {
+    const { asks } = this.props.poloniex.poloniexData;
+    const sellData = asks.map((order) => {
+      return {
+        x: parseFloat(order[0]),
+        y: order[1],
+      };
+    });
+
+    return sellData;
+  }
+
+
+  combineData(bittrexBuy, poloniexBuy, bittrexSell, poloniexSell) {
+    debugger;
+    return [ [...bittrexBuy, ...poloniexBuy], [...bittrexSell, ...poloniexSell] ]
+  }
+
+
   render() {
+    if (!this.props.bittrex.bittrexData) {
+      return <div>Loading...</div>
+    }
+
     return (
-      // <VictoryChart
-      //   theme={VictoryTheme.material}
-      //   animate={{ duration: 1000 }}
-      // >
-      //   <VictoryStack
-      //     colorScale={"blue"}
-      //   >
-      //     {this.state.data.map((data, i) => {
-      //       return (
-      //         <VictoryArea
-      //           key={i}
-      //           data={data}
-      //           interpolation={"basis"}
-      //         />
-      //       );
-      //     })}
-      //   </VictoryStack>
-      // </VictoryChart>
-      <div></div>
+      <VictoryChart
+        theme={VictoryTheme.material}
+        animate={{ duration: 1000 }}
+        domain={{ y: [0, 300] }}
+        height={300}
+        >
+          {this.combineData(this.bittrexBuy(), this.poloniexBuy(), this.bittrexSell(), this.poloniexSell()).map((data, i) => {
+            console.log(data)
+            return (
+              <VictoryLine
+                key={i}
+                data={data}
+                interpolation={"step"}
+                scale={{ x: "linear", y: "log" }}
+              />
+            );
+          })}
+      </VictoryChart>
     );
   }
 }
