161bb8213a32606af23edec01b1aa5f71b220512
David Ryan <djrdev@icloud.com>
2018-01-08 02:17:01 -0700

    added coinselection actions

---
+++ src/Actions/CoinSelection/CoinSelection.js
@@ -0,0 +1,18 @@
+import {
+  MAIN_COIN_CHANGE,
+  SECOND_COIN_CHANGE,
+} from '../../Utils/ActionTypes';
+
+export const SelectMain = (coin) => {
+  return {
+    type: MAIN_COIN_CHANGE,
+    payload: coin,
+  }
+}
+
+export const SelectSecond = (coin) => {
+  return {
+    type: SECOND_COIN_CHANGE,
+    payload: coin,
+  }
+}
