49b2fe5c75ed4da5ab43b91df67ca163d050409b
David Ryan <djrdev@icloud.com>
2018-03-09 09:56:43 -0700

    poloniex async awat

--- src/Actions/PoloniexDataFetch/PoloniexDataFetch.js
+++ src/Actions/PoloniexDataFetch/PoloniexDataFetch.js
@@ -6,31 +6,27 @@ import {
   FETCHING_POLONIEX_DATA_FAIL
 } from '../../Utils/ActionTypes';
 
-export const PoloniexDataFetch = (main, second) => {
-
-  return dispatch => {
-
-    dispatch(poloniexLoading());
-
-    return fetch(`https://poloniex.com/public?command=returnOrderBook&currencyPair=${main}_${second}`)
-      .then(res => res.json())
-      .then(res => {
-        return dispatch(poloniexSuccess(res));
-      })
-      .catch(error => {
-        return dispatch(poloniexFail(error));
-      });
-  };
+export const PoloniexDataFetch = (main, second) => async dispatch => {
+  dispatch(poloniexLoading());
+  try {
+    const res = await fetch(
+      `https://poloniex.com/public?command=returnOrderBook&currencyPair=${main}_${second}`
+    );
+    const endpoint = await res.json();
+    dispatch(poloniexSuccess(endpoint));
+  } catch (error) {
+    dispatch(poloniexFail(error));
+  }
 };
 
-export const poloniexFail = (error) => {
+export const poloniexFail = error => {
   return {
     type: FETCHING_POLONIEX_DATA_FAIL,
     payload: error
   };
 };
 
-export const poloniexSuccess = (result) => {
+export const poloniexSuccess = result => {
   return {
     type: FETCHING_POLONIEX_DATA_SUCCESS,
     payload: result
