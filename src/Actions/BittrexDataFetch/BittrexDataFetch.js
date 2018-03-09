be4afda0633a5c085926197db9cf7974cf7575d3
David Ryan <djrdev@icloud.com>
2018-03-09 09:55:36 -0700

    bittrex async await

--- src/Actions/BittrexDataFetch/BittrexDataFetch.js
+++ src/Actions/BittrexDataFetch/BittrexDataFetch.js
@@ -6,34 +6,31 @@ import {
   FETCHING_BITTREX_DATA_FAIL
 } from '../../Utils/ActionTypes';
 
-export const BittrexDataFetch = (main, second) => {
-  return dispatch => {
-    
-    dispatch(bittrexLoading());
-
-    return fetch(`https://bittrex.com/api/v1.1/public/getorderbook?market=${main}-${second}&type=both`)
-      .then(res => res.json())
-      .then(res => {
-        return dispatch(bittrexSuccess(res.result));
-      })
-      .catch(error => {
-        return dispatch(bittrexFail(error));
-      });
-  };
+export const BittrexDataFetch = (main, second) => async dispatch => {
+  dispatch(bittrexLoading());
+  try {
+    const res = await fetch(
+      `https://bittrex.com/api/v1.1/public/getorderbook?market=${main}-${second}&type=both`
+    );
+    const endpoint = await res.json();
+    dispatch(bittrexSuccess(endpoint));
+  } catch (error) {
+    dispatch(bittrexFail(error));
+  }
 };
 
-export const bittrexFail = (error) => {
+export const bittrexFail = error => {
   return {
     type: FETCHING_BITTREX_DATA_FAIL,
     payload: error
   };
 };
 
-export const bittrexSuccess = (result) => {
+export const bittrexSuccess = result => {
   return {
     type: FETCHING_BITTREX_DATA_SUCCESS,
     payload: result
-  }  ;
+  };
 };
 
 export const bittrexLoading = () => {
