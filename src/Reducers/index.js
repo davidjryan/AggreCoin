39c066cbec2ead68b7cf4663ecc0917a4100756e
David Ryan <djrdev@icloud.com>
2018-03-12 13:08:00 -0600

    GDAX reducer index.js

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import BittrexFetchReducer from './BittrexFetchReducer/BittrexFetchReducer';
 import PoloniexFetchReducer from './PoloniexFetchReducer/PoloniexFetchReducer';
 import CoinSelectReducer from './CoinSelectReducer/CoinSelectReducer';
+import GDAXOrderBookReducer from './GDAXOrderBookReducer/GDAXOrderBookReducer';
 
 export default combineReducers({
   coins: CoinSelectReducer,
   bittrex: BittrexFetchReducer,
-  poloniex: PoloniexFetchReducer
+  poloniex: PoloniexFetchReducer,
+  gdax: GDAXOrderBookReducer
 });
